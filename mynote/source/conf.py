b9568b12380672477da4d457f49e083c165f6034
Takashi Odaira <odaira@insightech.co.jp>
2020-07-05 22:40:52 +0900

    タイトル変更

--- mynote/source/conf.py
+++ mynote/source/conf.py
@@ -18,7 +18,7 @@ import sphinx_bootstrap_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'My Note'
+project = 'Odeilly\'s Note'
 copyright = '2020, Odeilly'
 author = 'Odeilly'
 
