00a4836b937e83bebee50692a811343915d95493
Takashi Odaira <odaira@insightech.co.jp>
2020-07-06 10:13:51 +0900

    plantumlの拡張を追加

--- mynote/source/conf.py
+++ mynote/source/conf.py
@@ -28,7 +28,12 @@ author = 'Odeilly'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo', 'sphinx.ext.githubpages']
+extensions = [
+    'sphinx.ext.mathjax',
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
+    'sphinxcontrib.plantuml',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
